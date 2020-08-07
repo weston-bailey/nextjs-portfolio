3ae9ab28de6a7896a4064522f0e4101a87cec92a
Weston Bailey <void@MBP.local>
2020-08-07 16:16:16 -0700

    added random testing pages

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/navbar'
+
+export default function About() {
+  return(
+        <div>
+          <Navbar />
+          <div>
+            <h1>Heyo</h1>
+          </div>
+        </div>
+  )
+}
